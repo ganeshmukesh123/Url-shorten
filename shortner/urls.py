072b2a74952c3f0764e39317b2bd2577d5f8bd6b
ganesh.c <ganesh.c@internal.media.net>
2021-04-09 16:14:44 +0530

    adding requirement.txtx file

--- shortner/urls.py
+++ shortner/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     
     re_path(r'^app$',views.index,name="index"),
     path('create',views.create,name="create"),
-    re_path(r'^\w+|',views.getActual,name="getActual")
+    # re_path(r'^\w+|',views.getActual,name="getActual")
+    path('<str:pk>',views.getActual,name="getActual")
 ]
